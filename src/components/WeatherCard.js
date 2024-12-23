11bd1a7dba9ce14f27d37218c6520bd23601016f
Suraj Malinga <malinga91@gmail.com>
2024-12-24 02:18:34 +0530

    Minor styles added

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,17 +1,19 @@
 import React from 'react'
 
 function WeatherCard({ data }) {
-    const { name, main, weather } = data;
+    const { name, main, weather, ...more } = data;
 
   return (
     <div>
-        <h2>{name}</h2>
-      <p>Temperature: {main.temp}°C</p>
-      <p>Condition: {weather[0].description}</p>
-      <img
+        <img
         src={`https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`}
         alt={weather[0].description}
       /> 
+      <h1>{main.temp}°C</h1>
+      <h2>{name}</h2>
+      <p>Condition: {weather[0].description}</p>
+      <p>Wind Speed: {more.wind.speed}</p>
+      
     </div>
   )
 }
